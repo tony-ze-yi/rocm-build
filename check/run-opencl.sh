9a96a2eb981e909818deb264243829281ee1d275
xuhuisheng <xyz20003@gmail.com>
2022-02-17 10:09:55 +0800

    update check

--- check/run-opencl.sh
+++ check/run-opencl.sh
@@ -1,5 +1,7 @@
 
-g++ -std=c++17 -o test_opencl src/test_opencl.cpp -g -lOpenCL -L/opt/rocm/opencl/lib -I/opt/rocm/opencl/include -O3
+mkdir -p build
 
-./test_opencl
+g++ -std=c++17 -o build/test_opencl src/test_opencl.cpp -g -lOpenCL -L/opt/rocm/opencl/lib -I/opt/rocm/opencl/include -O3
+
+./build/test_opencl
 
