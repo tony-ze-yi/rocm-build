6d5066a5a8f93a8e89b5490d0c3fbda204e92e6e
Tony Yi <24402057+tony-ze-yi@users.noreply.github.com>
2022-03-20 12:32:49 -0400

    fix: aws

--- env.sh
+++ env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export ROCM_INSTALL_DIR=/opt/rocm
-export ROCM_GIT_DIR=/home/work/ROCm
-export ROCM_BUILD_DIR=/home/work/rocm-build/build
-export ROCM_PATCH_DIR=/home/work/rocm-build/patch
-export AMDGPU_TARGETS="gfx803;gfx900;gfx906"
-export CMAKE_DIR=/home/work/local/cmake-3.16.8-Linux-x86_64
+export ROCM_GIT_DIR=/home/ubuntu/ROCm
+export ROCM_BUILD_DIR=/home/ubuntu/rocm-build/build
+export ROCM_PATCH_DIR=/home/ubuntu/rocm-build/patch
+export AMDGPU_TARGETS="gfx803;gfx900;gfx906;gfx1011"
+export CMAKE_DIR=/home/ubuntu/local/cmake-3.16.8-Linux-x86_64
 export PATH=$ROCM_INSTALL_DIR/bin:$ROCM_INSTALL_DIR/llvm/bin:$ROCM_INSTALL_DIR/hip/bin:$CMAKE_DIR/bin:$PATH
 
